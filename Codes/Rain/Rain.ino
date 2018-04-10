cca28d595f025e17f63dd9c49116abaff763e555
Op-Amp <32835503+Op-Amp@users.noreply.github.com>
2018-04-10 15:26:41 +0800

    Create Rain.ino

---
+++ Codes/Rain/Rain.ino
@@ -0,0 +1,45 @@
+#define RAINDROP A3
+
+int rain_detect() {
+  pinMode(2, OUTPUT);
+  
+  int rain, sum = 0;
+  while (i < 10) {
+    rain = analogRead(RAINDROP_AO);
+    sum += rain;
+    if (rain - (sum / (i + 1)) > 100 || rain - (sum / (i + 1)) < -100) {
+      i = 0;
+      sum = 0;
+      continue;
+    }
+    i++;
+  }
+  return sum / 10;
+
+  /*
+  //analog output
+  if(analogRead(RAINDROP_AO)<300) Serial.println("Heavy Rain");
+  else if(analogRead(RAINDROP_AO)<500) Serial.println("Moderate Rain");
+  else Serial.println("No Rain");
+
+  //digital output
+  if(digitalRead(RAINDROP_DO) == HIGH) Serial.println("No Rain Detected");
+  else Serial.println("Rain Detected"); */
+}
+
+int rain_print() {
+  int rain = rainDetect();
+  if(rain < 300) Serial.println("Heavy Rain");
+  else if(rain < 500) Serial.println("Moderate Rain");
+  else Serial.println("No Rain");
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(rain_detect());
+}
