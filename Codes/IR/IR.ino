cda39420e43a1a6e834096ee0812e279e94f6fb8
jw98 <limjitwei@gmail.com>
2018-04-13 20:20:13 +0800

    no message

--- Codes/IR/IR.ino
+++ Codes/IR/IR.ino
@@ -25,6 +25,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // Serial.println(infrared());
-  infrared();
+   Serial.println(infrared());
+//  infrared();
 }
